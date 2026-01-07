b152f8f6907f51b248e2cdf7dd3426946b04766d
Quickesthenry <henrypaulschaar@gmail.com>
2026-01-07 22:09:30 +0100

    Fixed issue with package system and added basic event and command registry structure

---
+++ src/main/java/net/quickesthenry/redstation/RedStationCore2/commands/PluginCommands.java
@@ -0,0 +1,15 @@
+package net.quickesthenry.redstation.RedStationCore2.commands;
+
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEvent;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEventType;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+
+public class PluginCommands {
+    public static void register(LifecycleEventManager lifeCycleEventManager) {
+        lifeCycleEventManager.registerEventHandler(LifecycleEvents.COMMANDS, PluginCommands::commandRegister);
+    }
+    public static void commandRegister(LifecycleEvent command) {
+        // Register commands here using brigadier api
+    }
+}
---
+++ src/main/java/net/quickesthenry/redstation/RedStationCore2/events/PluginEvents.java
@@ -0,0 +1,10 @@
+package net.quickesthenry.redstation.RedStationCore2.events;
+
+import org.bukkit.plugin.PluginManager;
+
+public class PluginEvents {
+    public static void register(PluginManager pluginManager) {
+        // register events here using pluginManager.registerEvents()
+
+    }
+}
