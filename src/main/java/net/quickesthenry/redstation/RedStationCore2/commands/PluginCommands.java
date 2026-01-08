8784e8c61e8d96f61d35241a90d0e0c0c8fb701e
Quickesthenry <henrypaulschaar@gmail.com>
2026-01-08 12:11:15 +0100

    Made utility classes final and added private constructor

--- src/main/java/net/quickesthenry/redstation/RedStationCore2/commands/PluginCommands.java
+++ src/main/java/net/quickesthenry/redstation/RedStationCore2/commands/PluginCommands.java
@@ -4,8 +4,12 @@ import io.papermc.paper.plugin.lifecycle.event.LifecycleEvent;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEventType;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import net.quickesthenry.redstation.RedStationCore2.events.PluginEvents;
 
-public class PluginCommands {
+public final class PluginCommands {
+    private PluginCommands()  {
+        throw new UnsupportedOperationException("Utility class");
+    }
     public static void register(LifecycleEventManager lifeCycleEventManager) {
         lifeCycleEventManager.registerEventHandler(LifecycleEvents.COMMANDS, PluginCommands::commandRegister);
     }
--- src/main/java/net/quickesthenry/redstation/RedStationCore2/events/PluginEvents.java
+++ src/main/java/net/quickesthenry/redstation/RedStationCore2/events/PluginEvents.java
@@ -2,7 +2,10 @@ package net.quickesthenry.redstation.RedStationCore2.events;
 
 import org.bukkit.plugin.PluginManager;
 
-public class PluginEvents {
+public final class PluginEvents {
+    private PluginEvents() {
+        throw new UnsupportedOperationException("Utility class");
+    }
     public static void register(PluginManager pluginManager) {
         // register events here using pluginManager.registerEvents()
 
